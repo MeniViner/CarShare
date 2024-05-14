73477dee0438c4665a9b9ec4b930b15473d44d0b
meniviner <meniviner@gmail.com>
2024-05-14 13:38:24 +0300

    .

--- src/config/firestore.jsx
+++ src/config/firestore.jsx
@@ -1,10 +1,8 @@
-
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-
 
 // Your web app's Firebase configuration
 // const firebaseConfig = {
@@ -16,22 +14,16 @@ import { getAnalytics } from "firebase/analytics";
 //   appId: process.env.REACT_APP_APP_ID
 // };
 
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBPGbd227eBPhTCWNP8tvBxFI8ndcxJuZ4",
-  authDomain: "car-share-react.firebaseapp.com",
-  projectId: "car-share-react",
-  storageBucket: "car-share-react.appspot.com",
-  messagingSenderId: "92813777463",
-  appId: "1:92813777463:web:c152b470da685afb533db2",
-  measurementId: "G-8E2HD380D8"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKUT,
+  messagingSenderId: process.env.REACT_APP_MESSEGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
 };
-
-// Initialize Firebase
-const analytics = getAnalytics(app);
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
+
