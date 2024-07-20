4cc728330336a8548952b56598f80cf38c9ec3db
MeniViner <meniviner@gmail.com>
2024-07-20 14:53:54 +0300

    .

--- src/data/firebaseConfig.jsx
+++ src/data/firebaseConfig.jsx
@@ -13,16 +13,6 @@ const firebaseConfig = {
   measurementId: process.env.REACT_APP_MEASUREMENT_ID
 };
 
-// const firebaseConfig = {
-//   apiKey: "AIzaSyAY6t3L5XfYOou-aMHO4GKpJ0dS-OIERHg",
-//   authDomain: "we-car-share.firebaseapp.com",
-//   projectId: "we-car-share",
-//   storageBucket: "we-car-share.appspot.com",
-//   messagingSenderId: "44167050649",
-//   appId: "1:44167050649:web:f637988d1905d83bbe4f08",
-//   measurementId: "G-N41GZRXK22"
-// };
-
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const auth = getAuth(app);
