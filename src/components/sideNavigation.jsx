56236d370433cc139684b4266fa073cb3ac04f15
MeniViner <meniviner@gmail.com>
2024-07-22 19:06:23 +0300

    .

--- src/components/sideNavigation.jsx
+++ src/components/sideNavigation.jsx
@@ -1,31 +1,23 @@
-
 import React, { useState, useEffect } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSearch} from '@fortawesome/free-solid-svg-icons';
+import { Link, useNavigate, useLocation } from 'react-router-dom';
+import '../styles/SideNavigation.css';
+
 import { RiMenu3Fill, RiMenuFoldLine } from "react-icons/ri";
 import { AiOutlineMessage } from "react-icons/ai";
-import { IoSettingsOutline, IoMapSharp, IoBookmarksOutline, IoListSharp,  IoPricetagsOutline  } from "react-icons/io5";
-import '../styles/SideNavigation.css';
-import { auth } from '../data/firebaseConfig'; // ייבוא האותנטיקציה מ-Firebase
-// import Icon from '@mdi/react';
+import { IoSettingsOutline, IoMapSharp, IoBookmarksOutline, IoListSharp,  IoPricetagsOutline, IoSearch } from "react-icons/io5";
+
+import { auth } from '../data/firebaseConfig';
+
 
 const SideNavigation = () => {
   const [isSideNavOpen, setIsSideNavOpen] = useState(false);
   const [user, setUser] = useState(null);
-
-  const navigate = useNavigate(); // הוספת useHistory בתוך הקומפוננטה
-  const [searchQuery, setSearchQuery] = useState(''); // הוספת מצב לחיפוש
-
-  const handleSearch = (e) => {
-    e.preventDefault();
-    closeSideNav();
-    navigate(`/utils/SearchResults?q=${searchQuery}`); // עדכון הנתיב בהתאם למיקום הקובץ
-  };
-  
+  const [searchQuery, setSearchQuery] = useState('');
+  const navigate = useNavigate();
+  const location = useLocation();
 
   useEffect(() => {
-    const storedUser = localStorage.getItem('user'); // קבלת פרטי המשתמש מה-LocalStorage
+    const storedUser = localStorage.getItem('user');
     if (storedUser) {
       setUser(JSON.parse(storedUser));
     }
@@ -46,58 +38,69 @@ const SideNavigation = () => {
   const toggleSideNav = () => setIsSideNavOpen(prev => !prev);
   const closeSideNav = () => setIsSideNavOpen(false);
 
+  const handleSearch = (e) => {
+    if (e.key === 'Enter') {
+      e.preventDefault();
+      closeSideNav();
+      navigate(`/utils/SearchResults?q=${searchQuery}`);
+    }
+  };
+
   return (
     <>
       <div className={`sidenav ${isSideNavOpen ? 'open' : ''}`}>
         <div className="side-menu">
-          {/* App Title and Logo */}
           <div className="side-menu-header">
             <h2>Car Share</h2>
           </div>
 
-          {/* Search Functionality */}
           <div className="side-menu-search">
-            <form onSubmit={handleSearch}>
-              <input
-                type="text"
-                placeholder="Search..."
-                value={searchQuery}
-                onChange={(e) => setSearchQuery(e.target.value)}
-              />
-              <button type="submit">
-                <FontAwesomeIcon icon={faSearch} />
-              </button>
-            </form>
+            <input
+              type="text"
+              placeholder={<><faSearch /> Search...</>}
+              value={searchQuery}
+              onChange={(e) => setSearchQuery(e.target.value)}
+              onKeyDown={handleSearch}
+            />
           </div>
 
-          {/* Navigation Links */}
-          <Link to="/map" className='side-menu-item' onClick={closeSideNav}>
-            <IoMapSharp className='icon'/>
+          <div className="input-with-icon">
+            <input
+              type="text"
+              placeholder="Search..."
+              value={searchQuery}
+              onChange={(e) => setSearchQuery(e.target.value)}
+              onKeyDown={handleSearch}
+            />
+          </div>
+
+          <Link to="/map" className={`side-menu-item ${location.pathname === '/map' ? 'active' : ''}`} onClick={closeSideNav}>
+            <IoMapSharp className='icon' />
             <h3>Map</h3>
           </Link>
-          <Link to="/saved" className='side-menu-item' onClick={closeSideNav}>
-            <IoBookmarksOutline  className='icon'/>
+          <Link to="/saved" className={`side-menu-item ${location.pathname === '/saved' ? 'active' : ''}`} onClick={closeSideNav}>
+            <IoBookmarksOutline className='icon' />
             <h3>Saved</h3>
           </Link>
-          <Link to="/car-list" className='side-menu-item' onClick={closeSideNav}>
-            <IoListSharp className='icon'/>
+          <Link to="/car-list" className={`side-menu-item ${location.pathname === '/car-list' ? 'active' : ''}`} onClick={closeSideNav}>
+            <IoListSharp className='icon' />
             <h3>Car List</h3>
           </Link>
-          <Link to="/prices" className='side-menu-item' onClick={closeSideNav}>
-            <IoPricetagsOutline className='icon'/>
+          <Link to="/prices" className={`side-menu-item ${location.pathname === '/prices' ? 'active' : ''}`} onClick={closeSideNav}>
+            <IoPricetagsOutline className='icon' />
             <h3>Prices</h3>
           </Link>
           <div className="smi2">
-            <Link to="/contact-info" className='side-menu-item ' onClick={closeSideNav}>
-              <AiOutlineMessage  className='icon'/>
+            <Link to="/contact-info" className={`side-menu-item ${location.pathname === '/contact-info' ? 'active' : ''}`} onClick={closeSideNav}>
+              <AiOutlineMessage className='icon' />
               <h3>Contact</h3>
             </Link>
-            <Link to="/settings" className='side-menu-item' onClick={closeSideNav}>
-              <IoSettingsOutline className='icon'/>
+            <Link to="/settings" className={`side-menu-item ${location.pathname === '/settings' ? 'active' : ''}`} onClick={closeSideNav}>
+              <IoSettingsOutline className='icon' />
               <h3>Settings</h3>
             </Link>
           </div>
-          {/* Profile Section */}
+
           <div className="side-menu-profile">
             {user ? (
               <Link to="/profile" onClick={closeSideNav}>
@@ -112,7 +115,7 @@ const SideNavigation = () => {
                 <img src='images/user.png' alt="User Avatar" className="profile-photo" />
                 <div className="profile-details">
                   <p className="profile-name">Hi Guest</p>
-                  <p className="profile-email">log in here</p>
+                  <p className="profile-email">Log in here</p>
                 </div>
               </Link>
             )}
@@ -122,7 +125,7 @@ const SideNavigation = () => {
 
       <div className="side-nav-bt">
         <div className="left-icon" onClick={toggleSideNav}>
-          {isSideNavOpen ? <RiMenuFoldLine  /> : <RiMenu3Fill  />}
+          {isSideNavOpen ? <RiMenuFoldLine /> : <RiMenu3Fill />}
         </div>
       </div>
 
