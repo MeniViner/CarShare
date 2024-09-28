50029795803ca6db9e68316577c77f5a8f39bd04
MeniViner <meniviner@gmail.com>
2024-09-28 13:26:15 +0300

    .

--- src/components/top.jsx
+++ src/components/top.jsx
@@ -1,39 +1,13 @@
-import React, { useState, useEffect } from "react";
-import { auth } from "../data/firebaseConfig"; 
-import { onAuthStateChanged } from "firebase/auth";
+import React from "react";
 import SideNavigation from "./sideNavigation";
 
-
 const Top = () => {
-  // const [userName, setUserName] = useState("");
-
-  // useEffect(() => {
-  //   // מאזין לשינויים במצב המשתמש
-  //   const unsubscribe = onAuthStateChanged(auth, (user) => {
-  //     if (user) {
-  //       setUserName(user.displayName || user.email); // הגדרת שם המשתמש או הדוא"ל
-  //     } else {
-  //       setUserName(""); // אם אין משתמש מחובר
-  //     }
-  //   });
-
-  //   return () => unsubscribe(); // ניקוי  כשהקומפוננטה מוסרת
-  // }, []);
-
   return (
-    <>
-      <div className="top-section">
-        {/* <Link to="/">
-          <IoMdArrowRoundBack />
-        </Link> */}
-        <div className="side-nav-bt" >
-            <SideNavigation/>
-          </div>
-        {/* <span>
-          {userName ? `welcome back, ${userName}.` : "welcome back user"} 
-        </span> */}
-      </div>
-    </>
+    <div className="top-section">
+      <div className="side-nav-bt" >
+          <SideNavigation/>
+        </div>
+    </div>
   );
 };
 
--- src/index.js
+++ src/index.js
@@ -1,16 +1,3 @@
-// import React from "react";
-// import ReactDOM from "react-dom/client";
-// import App from "./App";
-// import './utils/i18next'; //this is for the translate 
-// import i18n from './utils/i18next'; // import i18n for language change detection
-
-// document.documentElement.dir = i18n.language === 'he' ? 'rtl' : 'ltr';
-// i18n.on('languageChanged', (lng) => {
-//   document.documentElement.dir = lng === 'he' ? 'rtl' : 'ltr';
-// });
-
-// ReactDOM.createRoot(document.getElementById('root')).render(<App />);
-
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
@@ -18,19 +5,12 @@ import './utils/i18next';
 import i18n from './utils/i18next';
 import './styles/theme.css';
 
-
-
 const applyStoredSettings = () => {
   const storedLanguage = localStorage.getItem('language') || 'he'; // ברירת מחדל לעברית אם אין שפה מוגדרת
-  const storedFontSize = localStorage.getItem('fontSize');
   const storedTheme = localStorage.getItem('theme');
 
   i18n.changeLanguage(storedLanguage);
   document.documentElement.dir = storedLanguage === 'he' ? 'rtl' : 'ltr';
-
-  if (storedFontSize) {
-    document.body.className = `font-size-${storedFontSize}`;
-  }
 
   if (storedTheme) {
     document.documentElement.setAttribute('data-theme', storedTheme);
@@ -39,7 +19,6 @@ const applyStoredSettings = () => {
     document.documentElement.setAttribute('data-theme', 'light');
   }
 };
-
 
 // Apply stored settings
 applyStoredSettings();
