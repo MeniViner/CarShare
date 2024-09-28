9a94435baa516fb09a881643781fbca8bb350d85
MeniViner <meniviner@gmail.com>
2024-09-28 23:30:57 +0300

    .

--- src/data/firebaseConfig.jsx
+++ src/data/firebaseConfig.jsx
@@ -1,21 +1,3 @@
-// import { initializeApp } from "firebase/app";
-// import { getFirestore } from "firebase/firestore";
-// import { getAuth } from "firebase/auth";
-
-// const firebaseConfig = {
-//   apiKey: "AIzaSyAY6t3L5XfYOou-aMHO4GKpJ0dS-OIERHg",
-//   authDomain: "we-car-share.firebaseapp.com",
-//   projectId: "we-car-share",
-//   storageBucket: "we-car-share.appspot.com",
-//   messagingSenderId: "44167050649",
-//   appId: "1:44167050649:web:f637988d1905d83bbe4f08",
-//   measurementId: "G-N41GZRXK22"
-// };
-
-// const app = initializeApp(firebaseConfig);
-// export const db = getFirestore(app);
-// export const auth = getAuth(app);
-
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
